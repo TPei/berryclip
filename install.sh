2340ea618b6cc3a463e36b828db72fe25ca0ecef
root <root@raspberrypi.home>
2013-02-19 19:11:07 +0000

    Installed BerryClip scripts from Matt Hawkins

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+rm -f berryclip_??.py
+rm -f instructions.txt
+wget "http://www.raspberrypi-spy.co.uk/berryclip/6_led/berryclip_01.py" -w 2
+wget "http://www.raspberrypi-spy.co.uk/berryclip/6_led/berryclip_02.py" -w 2
+wget "http://www.raspberrypi-spy.co.uk/berryclip/6_led/berryclip_03.py" -w 2
+wget "http://www.raspberrypi-spy.co.uk/berryclip/6_led/berryclip_04.py" -w 2
+wget "http://www.raspberrypi-spy.co.uk/berryclip/6_led/berryclip_05.py" -w 2
+wget "http://www.raspberrypi-spy.co.uk/berryclip/6_led/berryclip_06.py" -w 2
+wget "http://www.raspberrypi-spy.co.uk/berryclip/6_led/berryclip_07.py" -w 2
+wget "http://www.raspberrypi-spy.co.uk/berryclip/6_led/berryclip_08.py" -w 2
+wget "http://www.raspberrypi-spy.co.uk/berryclip/6_led/berryclip_09.py" -w 2
+wget "http://www.raspberrypi-spy.co.uk/berryclip/6_led/instructions.txt" -w 2
